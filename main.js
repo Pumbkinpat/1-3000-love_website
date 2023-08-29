2b9c9bcfe86f0b0a60f2eadd54ba503c760f39e1
Pumbkinpat <97525522+Pumbkinpat@users.noreply.github.com>
2023-08-29 23:49:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function myLovecount() {
     var id = setInterval(myFunction,0);
     function myFunction() {    
             count++;
-            document.write( "  "+ "i love you" + " " + count + "  " )
+            document.write( "  "+ "i    love   you   " + " " + count )
+            document.write("</br>")
         if (count == 3000) clearInterval(id); 
     }
 }
-
 
